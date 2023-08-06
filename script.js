70fcbe2b54994f8310c1cddf6f82d25796d2f390
jess <home@jessicas-mbp.lan>
2023-08-06 19:51:23 -0400

    add js

---
+++ script.js
@@ -0,0 +1,23 @@
+// if (first condition) {
+//     // block of code to be executed if first condition is truthy
+//  } else if (second condition) {
+//     // block of code to be executed if second condition is truthy
+//  }
+
+var topics = "HTML, CSS, Git, JavaScript";
+
+for(var x=0; x< topics.length; x++);
+console.log(topics[x]);
+
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
